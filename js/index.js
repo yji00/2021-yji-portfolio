31ec3bca5197023bf8443c9a63d73ed36731810b
yji00 <dksro333@naver.com>
2021-06-21 17:52:32 +0900

    ..

--- js/index.js
+++ js/index.js
@@ -27,37 +27,49 @@ var $skew =$('.skew-wrapper');
 // 	$(this).css('transform', 'rotate('+rotate+'deg) scale('+scale+')')
 // });
 
-function mouse(e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  var coor = 'Coordinates: (' + x + ',' + y + ')';
-  console.log(coor);
-  console.log($skew);
-  if( x<1000 && y<1000 ) {
-    $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.006+'deg)');
-  }
-  else if( x<1000 && y>1000 ) $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.005+'deg)');
-  else if( x>1000 && y<1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, '+y*0.005+'deg)');
-  else if( x>1000 && y>1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, -'+y*0.005+'deg)');
-}
+// $('.black-wrapper').mousemove(mouse(e));
+// function mouse(e) {
+//   var x = e.clientX;
+//   var y = e.clientY;
+//   var coor = 'Coordinates: (' + x + ',' + y + ')';
+//   console.log(coor);
+//   console.log($skew);
+//   if( x<1000 && y<1000 ) {
+//     $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.006+'deg)');
+//   }
+//   else if( x<1000 && y>1000 ) $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.005+'deg)');
+//   else if( x>1000 && y<1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, '+y*0.005+'deg)');
+//   else if( x>1000 && y>1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, -'+y*0.005+'deg)');
+// }
 
-var $numwrap =$('.gradient-wrapper').find('.number')
-var num =$numwrap.text()
 
-var memberCountConTxt= $numwrap.text();
+
+var $title = $('ul.skill-wrap');
+
+
+function onSkillClick() {
+  console.log(this)
+
+  $(this).addClass('active')
+  var $numwrap =$('.gradient-wrapper').find('.number')
+  var num =$numwrap.text()
   
-$({ val : 0 }).animate({ val : memberCountConTxt }, {
- duration: 2000,
-step: function() {
-  var num = numberWithCommas(Math.floor(this.val));
-  $numwrap.text(num);
-},
-complete: function() {
-  var num = numberWithCommas(Math.floor(this.val));
-  $numwrap.text(num);
+  var memberCountConTxt= $numwrap.text();
+    
+  $({ val : 0 }).animate({ val : memberCountConTxt }, { duration: 2000, 
+    step: function() {
+    var num = numberWithCommas(Math.floor(this.val));
+    $numwrap.text(num);
+  },
+  complete: function() {
+    var num = numberWithCommas(Math.floor(this.val));
+    $numwrap.text(num);
+  }
+  });
+  
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 }
-});
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+$title.find('li').click(onSkillClick);
