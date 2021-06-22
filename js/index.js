7e459af27f991dc833e3911abbfd5356f83ade3e
yji00 <dksro333@naver.com>
2021-06-22 22:32:13 +0900

    contact 애니 추가 wow추가

--- js/index.js
+++ js/index.js
@@ -1,45 +1,14 @@
-var cnt = 0;
+
+
+(function() {
+
+  new WOW().init();
+
+  var cnt = 0;
 var elm = ".main";
-var $skew = $('.skew-wrapper');
-var delay = 2000;
+var $skew =$('.skew-wrapper');
+var $title = $('ul.skill-wrap');
 
-var now = -1;
-var isScroll = true;
-var $section = $('.gnb-wrapper > section');
-var sectionLast = $section.length - 1;
-var zIdx = 11;
-
-/*
-$(window).on("mousewheel DOMMouseScroll", function (e) {
-	e.preventDefault();
-	if (isScroll) {
-		isScroll = false;
-		var delta = 0;
-		if (!event) event = window.event;
-		if (event.wheelDelta) {
-			delta = event.wheelDelta / 120;
-			if (window.opera) delta = -delta;
-		}
-		else if (event.detail)
-			delta = -event.detail / 3;
-		// 마우스휠을 위에서 아래로
-		if (delta < 0) {
-			now = (now == sectionLast) ? 0 : now + 1;
-			$section.eq(now).css('z-index', zIdx++);
-			$section.eq(now).addClass('active')
-		}
-		else { // 마우스휠을 아래에서 위로
-			now = (now == 0) ? sectionLast : now - 1;
-			$section.eq(now).css('z-index', zIdx++);
-			$section.eq(now).removeClass('active')
-		}
-		setTimeout(function () {
-			isScroll = true;
-			//$section.removeClass('active')
-		}, delay)
-	}
-});
-*/
 
 // 개별적으로 Wheel 이벤트 적용
 // $(elm).on("mousewheel DOMMouseScroll", function (e) {
@@ -61,54 +30,139 @@ $(window).on("mousewheel DOMMouseScroll", function (e) {
 // 	}
 // 	var rotate = cnt ;
 // 	var scale = 1 - (cnt / 100);
-
+	
 // 	console.log(cnt);
 // 	$(this).css('transform', 'rotate('+rotate+'deg) scale('+scale+')')
 // });
 
- $('.black-wrapper').mousemove(mouse);
- function mouse(e) {
-   var x = e.clientX;
-   var y = e.clientY;
-   var coor = 'Coordinates: (' + x + ',' + y + ')';
-   console.log(coor);
-   console.log($skew);
-   if( x<1000 && y<1000 ) {
-     $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.006+'deg)');
-   }
-   else if( x<1000 && y>1000 ) $skew.css('transform', 'skew( -'+x*0.006+'deg, -'+y*0.005+'deg)');
-   else if( x>1000 && y<1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, '+y*0.005+'deg)');
-   else if( x>1000 && y>1000 ) $skew.css('transform', 'skew( '+x*0.005+'deg, -'+y*0.005+'deg)');
- }
+// function mouse(e) {
+//   var x = e.clientX;
+//   var y = e.clientY;
+//   var coor = 'Coordinates: (' + x + ',' + y + ')';
+//   console.log(coor);
+//   console.log($skew);
+//   $skew.css('transform', 'perspective('+x*0.1+'px)');
+  
+// }
+// $('.black-wrapper').mousemove(mouse);
 
 
-var $title = $('ul.skill-wrap');
 
 
 function onSkillClick() {
-	console.log(this)
-	
-	$(this).addClass('active')
-	var $numwrap = $('.gradient-wrapper').find('.number')
-	var num = $numwrap.text()
-	
-	var memberCountConTxt = $numwrap.text();
-	
-	$({ val: 0 }).animate({ val: memberCountConTxt }, {
-		duration: 2000,
-		step: function () {
-			var num = numberWithCommas(Math.floor(this.val));
-			$numwrap.text(num);
-		},
-		complete: function () {
-			var num = numberWithCommas(Math.floor(this.val));
-			$numwrap.text(num);
-		}
-	});
-	
-	function numberWithCommas(x) {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+  console.log(this)
+  var skillIndex = $(this).index() 
+  var $numwrap = $('.gradient-wrapper').find('.number')
+  var numarr = [95, 80, 90]
+  var descarr = ['Lorem ipsum dolor sit amet consectetur adipisicing elit.','The discovery of the texts origin is attributed to Richard McClintock,', ' Consequatur cupiditate ad inventore ex eveniet at fugiat,']
+  console.log(  $('.desc'))
+  // $('.desc').removeClass('fadeInUp')
+  $('.desc').text(descarr[skillIndex]).show().removeClass('fadeInUp').addClass('fadeInUp')
+
+  
+  $title.find('li').removeClass('active')
+  $(this).addClass('active')
+
+  
+  var memberCountConTxt= numarr[skillIndex]
+
+    
+  $({ val : 0 }).animate({ val : memberCountConTxt }, { duration: 2000, 
+    step: function() {
+    var num = numberWithCommas(Math.floor(this.val));
+    $numwrap.text(num);
+  },
+  complete: function() {
+    var num = numberWithCommas(Math.floor(this.val));
+    $numwrap.text(num);
+  }
+
+  });
+  
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 }
 
 $title.find('li').click(onSkillClick);
+  // Init
+  var container = document.getElementById("container"),
+      inner = document.getElementById("inner");
+
+
+  // Mouse
+  var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function(event) {
+      var e = event || window.event;
+      this.x = e.clientX - this._x;
+      this.y = (e.clientY - this._y) * -1;
+      console.log(this.x);
+    },
+    setOrigin: function(e) {
+      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function() {
+      return "(" + this.x + ", " + this.y + ")";
+    }
+  };
+
+  // Track the mouse position relative to the center of the container.
+  mouse.setOrigin(container);
+
+  //----------------------------------------------------
+
+  var counter = 0;
+  var refreshRate = 10;
+  var isTimeToUpdate = function() {
+    return counter++ % refreshRate === 0;
+  };
+
+  //----------------------------------------------------
+
+  var onMouseEnterHandler = function(event) {
+    update(event);
+  };
+
+  var onMouseLeaveHandler = function() {
+    inner.style = "";
+  };
+
+  var onMouseMoveHandler = function(event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  //----------------------------------------------------
+
+  var update = function(event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+      (mouse.y / inner.offsetHeight / 2).toFixed(2),
+      (mouse.x / inner.offsetWidth / 2).toFixed(2)
+    );
+  };
+
+  var updateTransformStyle = function(x, y) {
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTranform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+  };
+
+  //--------------------------------------------------------
+
+  container.onmousemove = onMouseMoveHandler;
+  container.onmouseleave = onMouseLeaveHandler;
+  container.onmouseenter = onMouseEnterHandler;
+}
+
+
+)();
